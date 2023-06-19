7ae16f79d36ab9aa1355a068b4cabb79b2ebc20d
sillah0 <siladennis624@gmail.com>
2023-06-19 13:36:13 -0700

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
