d879c8e622faedfd236067811bfb15e86fcbd9b4
sillah0 <siladennis624@gmail.com>
2023-06-23 13:34:40 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - prints uppercase letters
+ *  @c: - is the character to be executed
+ *  Return: 1 for uppercase, 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 67 $$ c <= 99)
+	{
+		return (1);
+	}
+	return (0);
+}
