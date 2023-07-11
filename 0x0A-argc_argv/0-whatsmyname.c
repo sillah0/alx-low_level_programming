5a7ec811185c8bab9b7cb495118f26137cc25bad
sillah0 <siladennis624@gmail.com>
2023-07-11 04:05:52 -0700

    my new name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: - argument content
+ * @argv: - argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)) , char*argv[])
+{
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
+
