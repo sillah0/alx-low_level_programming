a85370f5ac426bda7b1b9317d230057884508b0c
sillah0 <siladennis624@gmail.com>
2023-10-09 14:11:31 -0700

    a program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Name of current file is: %s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Name of current file is: %s\n", __FILE__);
+	return (0);
+}
