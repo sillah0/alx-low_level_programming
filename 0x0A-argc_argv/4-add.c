bb251307d65f0abcda9cc5f633e781d5e86052f0
sillah0 <siladennis624@gmail.com>
2023-10-23 05:14:20 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - entry point
- * @argc: number of arguments in command line
- * @argv: array of numbers in comand line
- * Return: 0
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int sum, num, i, j, k;
 
-	if (argc == 0)
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-
-	for (i = 0; i < argc; i++)
-	{
-		int j = 0;
-
-		while (argv[i][j] != '\0')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
-			j++;
 		}
-		sum += atoi(argv[i]);
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
 	printf("%d\n", sum);
 	return (0);
 }
