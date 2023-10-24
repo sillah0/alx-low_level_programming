bd830f3745fd288100a46b40517c90adaf1de624
sillah0 <siladennis624@gmail.com>
2023-10-24 00:19:37 -0700

    function to free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocation
+ * @d: pointer to struct dog
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free((*d).name);
+		free((*d).name);
+		free(d);
+	}
+}
