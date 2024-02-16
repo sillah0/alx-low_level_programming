25408b13a8a028ecaa0e23035b7f2c6866be91b7
sillah0 <siladennis624@gmail.com>
2024-02-17 01:18:20 +0300

    updated

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_value - gives a index of a key in the hash table array
+ * @key: key
+ * @size: size of array
+ * 
+ * Return:index to store value
+*/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value;
