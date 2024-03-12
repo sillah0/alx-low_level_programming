480e2bcd763a485eb841a54ce1d9660c89b7f81c
sillah0 <siladennis624@gmail.com>
2024-03-12 16:44:19 +0300

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,9 +13,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	char *value_copy;
 	unsigned long int indx, n;
-	
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
@@ -32,10 +33,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
-			return (0);
+		return (0);
 	}
 	new->value = value_copy;
 	new->next = ht->array[indx];
